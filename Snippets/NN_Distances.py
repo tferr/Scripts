2f59fa80cdfc8f5eb060d79911b7311fecdb60a0
Tiago Ferreira <tiagoalvespedrosa@gmail.com>
2015-01-01 15:08:56 -0500

    NND: No need to raise a RuntimeException to exit the script Instead, we can just set variables to None which improves readability and will make more sense for users

--- Snippets/NN_Distances.py
+++ Snippets/NN_Distances.py
@@ -4,12 +4,11 @@
 # Calculates the closest pair of points from a 2D/3D list of centroid coordinates (opened in the IJ
 # 'Results' table) calling another BAR script to plot the frequencies of nearest neighbor distances.
 #
-# TF 20141201
+# TF 20150101
 
 import math, sys
 from ij import IJ, Menus
 import ij.measure.ResultsTable as RT
-from java.lang import Exception, RuntimeException
 
 # Specify column headings listing x,y,z positions
 xHeading, yHeading, zHeading = "X", "Y", "Z"
@@ -17,44 +16,48 @@ xHeading, yHeading, zHeading = "X", "Y", "Z"
 # Retrieve Results Table
 rt = RT.getResultsTable()
 
-# Retrive x,y positions. Exit if columns are not found
+# Retrive x,y positions
 try:
     x = rt.getColumn(rt.getColumnIndex(xHeading))
     y = rt.getColumn(rt.getColumnIndex(yHeading))
-except Exception, e:
-    IJ.error("Invalid Results Table","Data for X,Y positions not found.")
-    raise RuntimeException("Invalid Results Table: "+ str(e))
+except:
+    x = y = None
 
-# Retrive z positions. Ignore positions if column is not found
-try:
-    z = rt.getColumn(rt.getColumnIndex(zHeading))
-except Exception, e:
-    IJ.log("Z-column not found: Assuming 2D distances...")
-    z = [0]*len(x)
+if not None in (x, y):
 
-# Calculate distances for all positions. Retrieve NNs distances
-for i in range(len(x)):
-    minDx = sys.maxint
-    nearest = 0
-    for j in range(len(x)):
-        if i==j: continue
-        dx = (x[i]-x[j])**2
-        dy = (y[i]-y[j])**2
-        dz = (z[i]-z[j])**2
-        dst = math.sqrt(dx+dy+dz)
-        if dst>0 and dst<minDx:
-            minDx = dst
-            nearest = j+1
-    rt.setValue("NN pair", i, nearest)
-    rt.setValue("NN distance", i, minDx);
+    # Retrive z positions. Ignore positions if column is not found
+    try:
+        z = rt.getColumn(rt.getColumnIndex(zHeading))
+    except:
+        IJ.log("Z-column not found: Assuming 2D distances...")
+        z = [0]*len(x)
 
-# Display appended results
-rt.showRowNumbers(True)
-rt.show("Results")
+    # Calculate distances for all positions. Retrieve NNs distances
+    for i in range(len(x)):
+        minDx = sys.maxint
+        nearest = 0
+        for j in range(len(x)):
+            if i==j: continue
+            dx = (x[i]-x[j])**2
+            dy = (y[i]-y[j])**2
+            dz = (z[i]-z[j])**2
+            dst = math.sqrt(dx+dy+dz)
+            if dst>0 and dst<minDx:
+                minDx = dst
+                nearest = j+1
+        rt.setValue("NN pair", i, nearest)
+        rt.setValue("NN distance", i, minDx);
 
-# Display distributions
-dp = "Distribution Plotter"
-if dp in Menus.getCommands().keySet().toArray():
-    IJ.run(dp, "parameter=[NN distance] automatic=Freedman-Diaconis");
+    # Display appended results
+    rt.showRowNumbers(True)
+    rt.show("Results")
+
+    # Display distributions
+    dp = "Distribution Plotter"
+    if dp in Menus.getCommands().keySet().toArray():
+        IJ.run(dp, "parameter=[NN distance] automatic=Freedman-Diaconis");
+    else:
+        IJ.error("File missing", dp+" not found.\nPlease check your BAR installation.")
+
 else:
-    IJ.error("File missing", dp+" not found.\nPlease check your BAR installation.")
+    IJ.error("Invalid Results Table","Data for X,Y positions not found.")
