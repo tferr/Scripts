1afbf8a407e83dbf4a0da548f439169088755106
Tiago Ferreira <tiagoalvespedrosa@gmail.com>
2015-01-01 17:53:05 -0500

    Symlink newer directories (While at it, simplify things by using mkdir -p)

--- misc/symlink_bar.sh
+++ misc/symlink_bar.sh
@@ -1,55 +1,47 @@
 #!/bin/bash
 ## IJ BAR: https://github.com/tferr/Scripts#scripts
 ## Symlinks files in the repository to a Fiji installation
-## TF 2014.06
+## TF 2015.01
 
 ## Source and destination paths
     SRC_DIR=`dirname $(pwd -P)`
     FIJI_HOME="/Applications/Fiji/Fiji.app"
     BAR_DIR="$FIJI_HOME/plugins/Scripts/BAR"
 
-
-## 1. Symlink Data_Analysis/ files (create $BAR_DIR if needed)
-    DIR="$BAR_DIR/Data_Analysis"
-    if [ ! -d $DIR ]; then
-        mkdir -p $DIR
-    fi
-    ln -f -s $SRC_DIR/Data_Analysis/* $DIR
-
-## 2. Symlink Annotation/ files
+## 1. Symlink Annotation/ files
     DIR="$BAR_DIR/Annotation"
-    if [ ! -d $DIR ]; then
-        mkdir $DIR
-    fi
-    ln -f -s $SRC_DIR/Annotation/* $DIR
+    mkdir -p $DIR
+    ln -fs $SRC_DIR/Annotation/* $DIR
+
+## 2. Symlink Data_Analysis/ files
+    DIR="$BAR_DIR/Data_Analysis"
+    mkdir -p $DIR
+    ln -fs $SRC_DIR/Data_Analysis/* $DIR
 
 ## 3. Symlink Morphometry/ files
     DIR="$BAR_DIR/Morphometry"
-    if [ ! -d $DIR ]; then
-        mkdir $DIR
-    fi
-    ln -f -s $SRC_DIR/Morphometry/* $DIR
+    mkdir -p $DIR
+    ln -fs $SRC_DIR/Morphometry/* $DIR
 
 ## 4. Symlink Segmentation/ files
     DIR="$BAR_DIR/Segmentation"
-    if [ ! -d $DIR ]; then
-        mkdir $DIR
-    fi
-    ln -f -s $SRC_DIR/Segmentation/* $DIR
+    mkdir -p $DIR
+    ln -fs $SRC_DIR/Segmentation/* $DIR
 
-## 5. Symlink macro tools
+## 5. Symlink Snippets/ files
+    DIR="$BAR_DIR/Snippets"
+    mkdir -p $DIR
+    ln -fs $SRC_DIR/Segmentation/* $DIR
+
+## 6. Symlink macro tools
     DIR="$FIJI_HOME/macros/tools"
-    if [ ! -d $DIR ]; then
-        mkdir $DIR
-    fi
-    ln -f -s $SRC_DIR/Tools/*.ijm $DIR
+    mkdir -p $DIR
+    ln -fs $SRC_DIR/Tools/*.ijm $DIR
 
-## 6. Symlink macro toolsets
+## 7. Symlink macro toolsets
     DIR="$FIJI_HOME/macros/toolsets"
-    if [ ! -d $DIR ]; then
-        mkdir $DIR
-    fi
-    ln -f -s $SRC_DIR/Tools/Toolsets/*.ijm $DIR
+    mkdir -p $DIR
+    ln -fs $SRC_DIR/Tools/Toolsets/*.ijm $DIR
 
-## 7. Symlink compiled .jar
-    ln -f -s $SRC_DIR/BAR/target/*.jar $FIJI_HOME/plugins/
+## 8. Symlink compiled .jar
+    ln -fs $SRC_DIR/BAR/target/*.jar $FIJI_HOME/plugins/
