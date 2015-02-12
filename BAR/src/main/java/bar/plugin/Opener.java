ccfbe21c3d67cdf5e832b139476e36b8dbb66327
Tiago <tiagoalvespedrosa@gmail.com>
2015-02-12 16:08:06 -0500

    Add missing imports

--- BAR/src/main/java/bar/plugin/Opener.java
+++ BAR/src/main/java/bar/plugin/Opener.java
@@ -12,10 +12,16 @@
 package bar.plugin;
 
 import fiji.Debug;
+import fiji.util.gui.GenericDialogPlus;
 import ij.IJ;
+import ij.gui.DialogListener;
 import ij.gui.GenericDialog;
+import ij.io.OpenDialog;
 import ij.plugin.PlugIn;
+import ij.text.TextPanel;
+import ij.text.TextWindow;
 
+import java.awt.AWTEvent;
 import java.awt.Button;
 import java.awt.Color;
 import java.awt.Dialog;
@@ -27,6 +33,7 @@ import java.awt.MenuItem;
 import java.awt.Panel;
 import java.awt.PopupMenu;
 import java.awt.Rectangle;
+import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -38,6 +45,7 @@ import java.awt.event.WindowListener;
 import java.io.File;
 import java.io.FileFilter;
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Vector;
 
 import javax.swing.JLabel;
