c60e24435f0122de375b16da8b2c1ef499018326
Tiago <tiagoalvespedrosa@gmail.com>
2015-03-02 12:44:40 -0500

    Implement missing ActionListener class for search history

--- BAR/src/main/java/bar/plugin/Commander.java
+++ BAR/src/main/java/bar/plugin/Commander.java
@@ -1589,6 +1589,21 @@ public class Commander implements PlugIn, ActionListener, DocumentListener,
 		return !(query.isEmpty() || query.equals(PROMPT_PLACEHOLDER));
 	}
 
+	/** Implements ActionListeners for the "previous searches" dropdown menu */
+	private class HistoryActionListener implements ActionListener {
+		public void actionPerformed(final ActionEvent e) {
+			final String cmd = e.getActionCommand();
+			if (cmd.equals("Save search")) {
+				final String newEntry = prompt.getText();
+				if (validQuery(newEntry) && !prevSearches.contains(newEntry))
+					prevSearches.add(prompt.getText());
+			} else if (cmd.equals("Clear searches")) {
+				prevSearches.clear();
+			} else {
+				prompt.setText(cmd);
+			}
+		}
+	}
 
 	/** Implements ActionListeners for the "options" dropdown menu */
 	private class OptionsActionListener implements ActionListener {
