eebd423eba2d140d3ebfb6045180d598b24d9bf8
Tiago <tiagoalvespedrosa@gmail.com>
2016-04-16 23:23:24 -0400

    Add List_BARs snippet

---
+++ Snippets/List_BARs.groovy
@@ -0,0 +1,9 @@
+/* List_BARs.groovy
+ * IJ BAR: https://github.com/tferr/Scripts#scripts
+ */
+
+import ij.plugin.CommandFinder
+
+// See http://javadoc.imagej.net/ImageJ1/ij/plugin/CommandFinder.html
+cf = new CommandFinder()
+cf.run("bar")
