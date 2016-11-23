aec7a3f3e1b128760c980168fff196e484cb974c
Tiago <tiagoalvespedrosa@gmail.com>
2016-11-23 17:09:44 -0500

    Polish code

--- Snippets/NN_Distances.py
+++ Snippets/NN_Distances.py
@@ -1,5 +1,6 @@
 #@Context context
 #@UIService uiService
+#@LogService logService
 
 # NN_Distances.py
 # IJ BAR snippet https://github.com/tferr/Scripts/tree/master/Snippets
@@ -11,63 +12,76 @@
 # TF 20150810
 
 import math, sys
-from ij import IJ
 from bar import Utils
 import ij.measure.ResultsTable as RT
 
-
-def plot_distributions():
-	from bar import Runner
-	runner = Runner(context)
-	runner.runIJ1Macro("Data_Analysis/Distribution_Plotter.ijm", "NN distance")
-	if not runner.scriptLoaded():
-		uiService.showDialog("Distribution of NN distances not plotted.\nCheck console for details", "Error")
-
-
-# Specify column headings listing x,y,z positions
+# Column headings listing x,y,z positions
 xHeading, yHeading, zHeading = "X", "Y", "Z"
 
-# Retrieve valid data from the Results table
-rt = Utils.getResultsTable();
 
-# Retrive x,y positions
-try:
-    x = rt.getColumn(rt.getColumnIndex(xHeading))
-    y = rt.getColumn(rt.getColumnIndex(yHeading))
-except:
-    x = y = None
+def plot_distributions():
+    from bar import Runner
+    runner = Runner(context)
+    runner.runIJ1Macro("Data_Analysis/Distribution_Plotter.ijm", "NN distance")
+    if not runner.scriptLoaded():
+        uiService.showDialog("Distribution of NN distances not plotted.\n"
+                           + "Check console for details", "Error")
 
-if not None in (x, y):
 
-    # Retrive z positions. Ignore positions if column is not found
+def distance(x1, y1, z1, x2, y2, z2):
+    """ Retrieves the distances betwenn two points """
+    dx = (x1-x2)**2
+    dy = (y1-y2)**2
+    dz = (z1-z2)**2
+    return math.sqrt(dx+dy+dz)
+
+
+def getXYZPositons(rt, xcol_header, ycol_header, zcol_header):
+    """ Retrieves valid data from the Results table """
     try:
-        z = rt.getColumn(rt.getColumnIndex(zHeading))
-    except:
-        IJ.log("Z-column not found: Assuming 2D distances...")
-        z = [0]*len(x)
+        x = y = z = None
+        x = rt.getColumn(rt.getColumnIndex(xcol_header))
+        y = rt.getColumn(rt.getColumnIndex(ycol_header))
+        z = rt.getColumn(rt.getColumnIndex(zcol_header))
+    finally:
+        return x, y, z
 
-    # Calculate distances for all positions. Retrieve NNs distances
+
+def calcNNDistances(rt, x, y, z=None):
+    """ Calculates NN distances and adds them to the Results table """
+    # Ignore Z positions?
+    if z is None:
+        logService.info("NN: Assuming 2D distances...")
+        z = [0]*len(x)
+    # Calculate distances for all positions. Retrieve NNs
     for i in range(len(x)):
         minDx = sys.maxint
         nearest = 0
         for j in range(len(x)):
-            if i==j:
+            if i == j:
                 continue
-            dx = (x[i]-x[j])**2
-            dy = (y[i]-y[j])**2
-            dz = (z[i]-z[j])**2
-            dst = math.sqrt(dx+dy+dz)
-            if dst>0 and dst<minDx:
+            dst = distance(x[i], x[j], y[i], y[j], z[i], z[j])
+            if dst > 0 and dst < minDx:
                 minDx = dst
                 nearest = j+1
         rt.setValue("NN pair", i, nearest)
         rt.setValue("NN distance", i, minDx)
 
-    # Display appended results
-    rt.showRowNumbers(True)
-    rt.show("Results")
 
-    plot_distributions()
+def main():
+    rt = Utils.getResultsTable()
+    if rt is None:
+        return
+    x, y, z = getXYZPositons(rt, xHeading, yHeading, zHeading)
+    if not None in (x, y):
+        # Do the calculations and sisplay appended results
+        calcNNDistances(rt, x, y, z)
+        rt.showRowNumbers(True)
+        rt.show("Results")
+        plot_distributions()
+    else:
+        uiService.showDialog("Data for X,Y positions not found.",
+                             "Invalid Results Table")
 
-else:
-    IJ.error("Invalid Results Table","Data for X,Y positions not found.")
+
+main()
