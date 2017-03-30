7faf5e49187af49d4fba3de59a6fd291d1022b3e
Tiago <tiagoalvespedrosa@gmail.com>
2017-03-30 16:13:42 -0400

    Add script command to install single IJ1 macro tools

---
+++ BAR/src/main/resources/scripts/BAR/Utilities/Install_Productivity_Menu....groovy
@@ -0,0 +1,14 @@
+//@String(label="Place which tool in the IJ toolbar?", choices={"Calibration Menu", "List Folder Menu", "Shortcuts Menu"}, style="radioButtonVertical") tool
+//@boolean(label="Open file after installation") open
+//@ImageJ ij
+//@UIService uiservice
+
+
+import bar.Runner
+
+runner = new Runner(ij.getContext())
+runner.installIJ1Macro("BAR/tools/${tool.replaceAll(" ", "")}.ijm", true)
+if (!runner.scriptLoaded())
+	uiservice.showDialog("Unfortunately installation failed. See console for details", "Error")
+else if (open)
+	runner.openLastLoadedResource()
