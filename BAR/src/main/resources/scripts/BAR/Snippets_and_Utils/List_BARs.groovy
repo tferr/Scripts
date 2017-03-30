d154bbd3af21ce6c3eeb1621932587bd373f38bf
Tiago <tiagoalvespedrosa@gmail.com>
2017-03-30 10:30:16 -0400

    Remove old scripts in outdated path

--- BAR/src/main/resources/scripts/BAR/Snippets_and_Utils/List_BARs.groovy
+++
@@ -1,9 +0,0 @@
-/* List_BARs.groovy
- * IJ BAR: https://github.com/tferr/Scripts#scripts
- */
-
-import ij.plugin.CommandFinder
-
-// See http://javadoc.imagej.net/ImageJ1/ij/plugin/CommandFinder.html
-cf = new CommandFinder()
-cf.run("BAR>") // Caveat: Root commands like "BAR Commander..." won't be listed
