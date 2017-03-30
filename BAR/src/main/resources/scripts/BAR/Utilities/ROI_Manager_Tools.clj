1e970c9325413d8614122c23e01f45e42680b686
Tiago <tiagoalvespedrosa@gmail.com>
2017-03-30 15:54:14 -0400

    Replace legacy plugins.config with scripts.Its Clojure time!

---
+++ BAR/src/main/resources/scripts/BAR/Utilities/ROI_Manager_Tools.clj
@@ -0,0 +1,11 @@
+;@Context context
+;@UIService uiService
+
+(let [runner (bar.Runner. context)]
+	(.installIJ1Macro runner "BAR/tools/ROIManagerTools.ijm" false)
+	(if-not (.scriptLoaded runner)
+		(.showDialog uiService "File could not be installed. See Console for details" "Error"))
+
+	;;; Uncomment next line to have the macro opened in the script editor ;;;
+	;(.openLastLoadedResource runner)
+)
