0cfa17b0999a85cb060794a36795f37668934cbc
Tiago <tiagoalvespedrosa@gmail.com>
2017-04-03 23:48:49 -0400

    Modernize template libs (bsh, groovy, ijm)

--- BAR/src/main/resources/lib/BARlib.groovy
+++ BAR/src/main/resources/lib/BARlib.groovy
@@ -1,30 +1,33 @@
+//@UIService uiservice
+
 /* BARlib.groovy
  * IJ BAR: https://github.com/tferr/Scripts#scripts
  *
- * Common BAR library (https://github.com/tferr/Scripts/tree/master/lib#lib) to be
- * placed in BAR/lib. This file can host functions to be used across your scripts.
- * To load these scripting additions, append the following to your Groovy files:
+ * Common BAR library (http://imagej.net/BAR#BAR_lib) to be placed in BAR/lib. This file
+ * can host functions/methods to be used across your scripts. To load these scripting
+ * additions, append the following to your Groovy files:
  *
  *    import bar.Utils
  *    file = new File(Utils.getLibDir() + "BARlib.groovy")
- *    BARlib = new GroovyClassLoader(getClass().getClassLoader()).parseClass(file)
+ *    BARlib = new GroovyClassLoader().parseClass(file)
  *
  * Then, initiate the BARlib class, calling methods as usual:
  *    lib = BARlib.newInstance()
  *    lib.confirmLoading()
  */
 
+import bar.Utils
+
 class BARlib {
 
-	///////////  UTILITIES  ///////////
 	/** Acknowledges accessibility to this file */
 	def confirmLoading() {
-		ij.IJ.showMessage("BAR lib successfully loaded!")
+		uiservice.showDialog("BAR lib successfully loaded!")
 	}
 
 	/** Returns text from the system clipboard or an empty string if no text was found */
 	def getClipboardText() {
-		bar.Utils.getClipboardText()
+		Utils.getClipboardText()
 	}
 
 	/** Returns a random uuid */
@@ -32,16 +35,14 @@ class BARlib {
 		java.util.UUID.randomUUID().toString()
 	}
 
-
-	///////////  CALCULATIONS  ///////////
 	/**
-	 * Smooths 1D data according to the specified window. Returns the original
-	 * data if window is not a positive integrer.
+	 * Smooths 1D data according to the specified window. Returns
+	 * the original data if window is not a positive integrer.
 	 */
 	def getSimpleMovingAverage(values, window) {
 		if (window<1 || !(window instanceof Integer))
 			return values
-		def svalues= new Object[values.size()]
+		def svalues = []
 		def lastI = values.size()-1
 		for (i in 0..lastI) {
         	svalues[i] = 0; def n = 0
