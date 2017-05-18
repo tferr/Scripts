b284ba527d7b7cb7d9868451275a8148b14dfc79
Tiago <tiagoalvespedrosa@gmail.com>
2017-05-18 15:27:05 +0000

    Acknowledge missing dependency from the new BIG-EPFL update site http://forum.imagej.net/t/epfl-big-plugins-moved-to-their-own-update-site/5102?u=tferr

--- BAR/src/main/resources/scripts/BAR/Analysis/Time_Series/Register_Against_Average.groovy
+++ BAR/src/main/resources/scripts/BAR/Analysis/Time_Series/Register_Against_Average.groovy
@@ -12,15 +12,28 @@ def loadLib(resourcePath) {
 	(GroovyObject) cls.newInstance()
 }
 
-utils = loadLib("shared/Utils.groovy")
-if (!utils.isSingleChannelTimeseq(imp, uiservice))
-	return
 
-stack = imp.getStack()
-avg = utils.projAVG(imp, 1, stack.getSize())
-stack.addSlice("AVG_ANCHOR", avg.getProcessor(), 0)
-imp.setSlice(1)
-for (method in ["Rigid Body", "Affine"])
-	IJ.run(imp, "StackReg", "transformation=[$method]")
-stack.deleteSlice(1)
-imp.setStack(stack)
+try {
+
+	Class.forName("StackRseg_");
+	utils = loadLib("shared/Utils.groovy")
+	if (!utils.isSingleChannelTimeseq(imp, uiservice))
+		return
+
+	stack = imp.getStack()
+	avg = utils.projAVG(imp, 1, stack.getSize())
+	stack.addSlice("AVG_ANCHOR", avg.getProcessor(), 0)
+	imp.setSlice(1)
+	for (method in ["Rigid Body", "Affine"])
+		IJ.run(imp, "StackReg", "transformation=[$method]")
+	stack.deleteSlice(1)
+	imp.setStack(stack)
+
+} catch(ClassNotFoundException e) {
+
+	uiservice.showDialog("<HTML><b>StackReg was not found.<br>"
+		+ "Please subscribe to the BIG-EPFL update site to install it.</b><br><br>"
+		+ "See https://imagej.net/Following_an_update_site for details<br>"
+		+ "on how to add an update site to your ImageJ installation.", "Dependency Missing");
+
+}
